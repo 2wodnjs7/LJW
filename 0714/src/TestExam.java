f20cf36fcc9c42e2ccd81b7f13d910715d4311f4
LJW <2wodnjs7@gmail.com>
2020-07-14 09:10:46 +0900

    TestExam.java commit

---
+++ 0714/src/TestExam.java
@@ -0,0 +1,15 @@
+
+public class TestExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			System.out.print(i+"\t");
+		}
+		System.out.println();
+		for(int j=10;j>=6;j--) {
+			System.out.print(j+"\t");
+		}
+	}
+
+}
